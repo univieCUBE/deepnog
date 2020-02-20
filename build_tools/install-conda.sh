bdc9cce7afef968bf1f2c7d64d92b47855a83df9
Roman Feldbauer <sci@feldbauer.org>
2020-02-20 14:10:48 +0100

    allow travis linux failures for now

--- build_tools/install-conda.sh
+++ build_tools/install-conda.sh
@@ -8,10 +8,7 @@ if [[ -d "$MINICONDA_DIR" ]] && [[ -e "$MINICONDA_DIR/bin/conda" ]]; then
 else # if it does not exist, we need to install miniconda
     rm -rf "$MINICONDA_DIR" # remove the directory in case we have an empty cached directory
 
-    if [[ "$TRAVIS_OS_NAME" == 'linux' ]] && [[ "$TRAVIS_PYTHON_VERSION" == 2* ]]; then
-        echo "WARNING: using Python2"
-        wget https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh;
-    elif [[ "$TRAVIS_OS_NAME" == 'linux' ]]; then
+    if [[ "$TRAVIS_OS_NAME" == 'linux' ]]; then
         wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
     elif [[ "$TRAVIS_OS_NAME" == 'osx' ]]; then
         wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
@@ -25,5 +22,6 @@ else # if it does not exist, we need to install miniconda
     conda info -a # for debugging
     echo "$TRAVIS_PYTHON_VERSION"
     conda create --yes -n test python="$TRAVIS_PYTHON_VERSION"
-    source activate test
+    conda activate test
+    python -c "import sys; print(sys.version)"
 fi
