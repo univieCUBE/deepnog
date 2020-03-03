1f2bf2576da8a67ce08151feab991931a4361c80
Roman Feldbauer <sci@feldbauer.org>
2020-03-03 14:26:39 +0100

    FIX Use correct Python version on Travis

--- build_tools/install-conda.sh
+++ build_tools/install-conda.sh
@@ -28,9 +28,3 @@ fi
 if [[ ! -e "${MINICONDADIR}/envs/test/bin/python${TRAVIS_PYTHON_VERSION}" ]]; then
     conda create --yes -n test python="$TRAVIS_PYTHON_VERSION"
 fi
-
-# Finally load test environment to make sure we're using the specified Python version
-conda init bash
-source "$HOME/miniconda/etc/profile.d/conda.sh"
-conda activate test
-python -c "import sys; print(sys.version)"
