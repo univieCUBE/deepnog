e28f97d3147e832cde34ef142155094c171b6ee4
Roman Feldbauer <roman@feldbauer.org>
2020-04-16 09:17:42 +0200

    Upgrade PyTorch nightly

--- build_tools/install-requirements.sh
+++ build_tools/install-requirements.sh
@@ -4,17 +4,25 @@ set -e
 
 MINICONDA_DIR="$HOME/miniconda"
 source "$HOME/miniconda/etc/profile.d/conda.sh"
-export PATH="${MINICONDA_DIR}/bin:$PATH"
-conda activate test
+# export PATH="${MINICONDA_DIR}/bin:$PATH"
+"$MINICONDA_DIR/bin/conda" activate test
 hash -r
 
+echo "which pip"
+which pip
+echo "which python"
+which python
+echo "which python3"
+which python3
+echo "PATH"
+echo "$PATH"
 
 # This is a work-around for a bug with Ubuntu Linux and PyTorch 1.4.
-# Fetch the nightly 1.6 builds, until the official release.
+# Fetch the nightly 1.5 builds, until the official release.
 if [[ "$TRAVIS_OS_NAME" == 'linux' ]]; then
   echo "install and upgrade PyTorch nightly"
   conda install --yes pytorch cpuonly -c pytorch-nightly
-  conda upgrade --yes pytorch cpuonly -c pytorch-nightly
+  # conda upgrade --yes pytorch cpuonly -c pytorch-nightly
   echo "pip installing required python packages"
   pip install -r requirements/travis_ubuntu.txt
   python -c "import torch; print(f'PyTorch version = {torch.__version__}')"
