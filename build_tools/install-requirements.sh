7956f56479c1f25143a5c88332e504e183f2584b
Roman Feldbauer <roman@feldbauer.org>
2020-04-16 08:25:52 +0200

    Upgrade PyTorch nightly

--- build_tools/install-requirements.sh
+++ build_tools/install-requirements.sh
@@ -10,11 +10,14 @@ hash -r
 
 
 # This is a work-around for a bug with Ubuntu Linux and PyTorch 1.4.
-# Fetch the nightly 1.5 builds, until the offcial release.
+# Fetch the nightly 1.6 builds, until the official release.
 if [[ "$TRAVIS_OS_NAME" == 'linux' ]]; then
+  echo "install and upgrade PyTorch nightly"
   conda install --yes pytorch cpuonly -c pytorch-nightly
+  conda upgrade --yes pytorch
   echo "pip installing required python packages"
   pip install -r requirements/travis_ubuntu.txt
+  python -c "import torch; print(f'PyTorch version = {torch.__version__}')"
 elif [[ "$TRAVIS_OS_NAME" == 'osx' ]]; then
   echo "pip installing required python packages"
   pip install -r requirements/dev.txt
