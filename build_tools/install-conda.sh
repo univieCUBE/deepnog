3117c2552e58512cb9f5ea11df5bbd0a804c6169
Roman Feldbauer <sci@feldbauer.org>
2020-11-05 14:10:40 +0100

    BLD fix Travis MacOS Python versions

--- build_tools/install-conda.sh
+++ build_tools/install-conda.sh
@@ -8,20 +8,23 @@ if [[ -d "$MINICONDA_DIR" ]] && [[ -e "$MINICONDA_DIR/bin/conda" ]]; then
 else # if it does not exist, we need to install miniconda
     rm -rf "$MINICONDA_DIR" # remove the directory in case we have an empty cached directory
 
+    echo "Downloading miniconda..."
     if [[ "$TRAVIS_OS_NAME" == 'linux' ]]; then
         wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
     elif [[ "$TRAVIS_OS_NAME" == 'osx' ]]; then
         wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
     fi
 
+    echo "Installing miniconda..."
     bash miniconda.sh -b -p "$MINICONDA_DIR"
     chown -R "$USER" "$MINICONDA_DIR"
     source "$HOME/miniconda/etc/profile.d/conda.sh"
     hash -r
     conda config --set always_yes yes --set changeps1 no
+    echo "Updating conda..."
     conda update -q conda
     conda info -a # for debugging
-    echo "$TRAVIS_PYTHON_VERSION"
+    echo "TRAVIS_PYTHON_VERSION=$TRAVIS_PYTHON_VERSION"
 fi
 
 # Is the specified Python version available? If not, install it.
