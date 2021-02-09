f98e4e6df75d96d07de62de354ce4c4fda024698
Roman Feldbauer <roman@feldbauer.org>
2021-02-09 11:19:40 +0100

    Regression test for #43

--- deepnog/learning/tests/test_training.py
+++ deepnog/learning/tests/test_training.py
@@ -15,6 +15,34 @@ Y_TRUE = np.array([[0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 0, 2, 
                     1, 1, 1, 1, 1, 1, 1, 1]])
 
 
+@pytest.mark.parametrize("iterable_dataset", [True, False])
+@pytest.mark.parametrize("shuffle", [True, False])
+def test_iterable_training(iterable_dataset: bool, shuffle: bool):
+    """ Regression test for https://github.com/univieCUBE/deepnog/pull/43"""
+    results = fit(
+        architecture="deepnog", module="deepnog", cls="DeepNOG",
+        training_sequences=DEEPNOG_TESTS/"data/test_training_COG0443.faa",
+        validation_sequences=DEEPNOG_TESTS/"data/test_validation_COG0443.faa",
+        training_labels=DEEPNOG_TESTS/"data/test_training_COG0443.csv",
+        validation_labels=DEEPNOG_TESTS/"data/test_validation_COG0443.csv",
+        iterable_dataset=iterable_dataset,
+        shuffle=shuffle,
+        n_epochs=1,
+        random_seed=123,
+    )
+    training_data = results.training_dataset
+    val_data = results.validation_dataset
+    if iterable_dataset:
+        assert training_data.file != val_data.file
+        assert training_data.labels_file != val_data.labels_file
+    else:
+        training_ids = set([x.id for x in training_data.sequences])
+        val_ids = set([x.id for x in val_data.sequences])
+        assert training_ids != val_ids
+    df = training_data.labels.merge(val_data.labels, how="inner", on="protein_id")
+    assert df.shape[0] == 0
+
+
 @pytest.mark.parametrize('batch_size', [4, ])
 @pytest.mark.parametrize('num_workers', [0, 2])
 def test_shuffled_training(batch_size, num_workers):
@@ -45,3 +73,5 @@ def test_shuffled_training(batch_size, num_workers):
         assert x.shape == (2, 30)
     np.testing.assert_equal(results.y_train_true.sum(), Y_TRUE.sum())  # order should be different
     np.testing.assert_equal(results.y_val_pred.sum(), Y_TRUE.sum())
+
+
